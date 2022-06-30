7d06e5fb877eba6c942d8d67b301a25b0fcfa8ef
waka-man <wtwestman@gmail.com>
2022-06-30 16:34:17 -0700

    arghhhhhh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 
-int main ()
+/**
+ * main - They make us do this on the first day of being
+ * exposed to C? How are the absolute beginners gonna figure this out?
+ * Return: Again, void
+ */
+
+int main (void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
 	puts(str);
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - This is a stupid description
+ * Return: I can't believe I have to explain this
+ */
+
+int main(void)
 {
 	char mychar;
 	int myint;
@@ -9,10 +14,10 @@ int main()
 	float myfloat;
 
 	printf("Size of a char: %ld byte(s)\n", sizeof(mychar));
-	printf("Size of a int: %ld byte(s)\n", sizeof(myint));
-	printf("Size of a long: %ld byte(s)\n", sizeof(mylong));
-	printf("Size of a long long: %ld byte(s)\n", sizeof(mydlong));
+	printf("Size of an int: %ld byte(s)\n", sizeof(myint));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(mylong));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(mydlong));
 	printf("Size of a float: %ld byte(s)\n", sizeof(myfloat));
 
-	return 0;
+	return (0);
 }
