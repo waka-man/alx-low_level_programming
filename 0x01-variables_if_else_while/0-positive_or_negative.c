b00d3a3099e949a74063c604b281d243e28c6381
waka-man <wtwestman@gmail.com>
2022-07-01 14:40:43 -0700

    tenefi Betty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,7 +7,6 @@
  * Return: Nothing is returned
  */
 
-
 int main(void)
 {
 	int n;
@@ -19,9 +18,9 @@ int main(void)
 		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
-		{
-			printf("%d is zero\n", n);
-		}
+	{
+		printf("%d is zero\n", n);
+	}
 	else
 	{
 		printf("%d is negative\n", n);
