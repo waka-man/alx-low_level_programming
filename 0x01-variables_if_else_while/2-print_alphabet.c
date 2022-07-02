6280ba7e4c1c8b97a9e1fe240710bb20bb500abb
waka-man <wtwestman@gmail.com>
2022-07-01 17:36:43 -0700

    BETTIYEWAYE

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,14 @@
 int main(void)
 {
 	char alpha = 'a';
+
 	while (alpha <= 'z')
 	{
 		putchar(alpha);
 		alpha++;
 	}
+
 	putchar('\n');
+
 	return (0);
 }
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,11 +9,13 @@
 int main(void)
 {
 	char alpha;
+
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 		if (alpha != 'q' && alpha != 'e')
 			putchar(alpha);
 	}
+
 	putchar('\n');
 	return (0);
 }
