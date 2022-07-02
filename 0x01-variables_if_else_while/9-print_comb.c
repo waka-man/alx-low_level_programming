f6d868c242753b6da6483a5b9bb799942e8c2475
waka-man <wtwestman@gmail.com>
2022-07-01 18:44:17 -0700

    corrections

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,17 +8,17 @@
 
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 48; i < 58; i++)
-    {
-        putchar(i);
-        if (i < 57)
-        {
-            putchar(44);
-            putchar(32);
-        }
-    }
-    putchar('\n');
-    return (0);
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
