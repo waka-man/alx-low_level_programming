ca45e7264b8da4ef312e1ce5f6d873758107b1d0
waka-man <wtwestman@gmail.com>
2022-07-07 19:43:34 -0700

    corr

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,13 +17,21 @@ int main(void)
 		{
 			printf("FizzBuzz ");
 		}
+		else if (i % 5 == 0)
+		{
+			if (i != 100)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("Buzz");
+			}
+
+		}
 		else if (i % 3 == 0)
 		{
 			printf("Fizz ");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
 		}
 		else
 		{
