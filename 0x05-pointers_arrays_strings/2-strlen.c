122c7b61deecb2aa1f263d4aa29f6b1cf015a630
waka-man <wtwestman@gmail.com>
2022-07-11 17:26:06 -0700

    minor typo

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,7 +3,7 @@
 /**
  * _strlen - returns the length of a string by taking the pointer
  * to the string as a param
- * @s - pointer to the string variable
+ * @s: pointer to the string variable
  * Return: the length of the string as int
  */
 
