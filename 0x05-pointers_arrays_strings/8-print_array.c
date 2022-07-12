87cd6f869efb234694a7596ebe42d4e2e8819d6f
waka-man <wtwestman@gmail.com>
2022-07-11 19:58:28 -0700

    shit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
 /**
- * print_array - prints a given number of array elements based on passed param n
- * which is the number of elements to be printed, as well as pointer to the arra
+ * print_array - prints a given number of array elements based on passed
+ * param n
+ * which is the number of elements to be printed, as well as pointer
+ * to the arra
  * @a: pointer to the array
  * @n: number of elements we want to print
  * Return: nothing is returned
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -18,5 +18,7 @@ char *_strcpy(char *dest, char *src)
 		index++;
 	}
 
+	dest[index] = '\0';
+
 	return (dest);
 }
