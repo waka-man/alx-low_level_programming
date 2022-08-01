105bacbb3ad37bd7b700a62d4911425e8ac0429d
waka-man <wtwestman@gmail.com>
2022-08-01 16:14:24 -0700

    commiting the correct file this time

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure resembling the animal dog
+ * @name: variable to hold the dog's name
+ * @age: variable to hold the dog's age
+ * @owner: variable to hold the name of the owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
